65fe3a06bd767b6c495e40fc3dd15fd1ba889add
dabsishJoy <dabasish@6sensehq.com>
2023-11-05 00:13:15 +0600

    added condition when list is empty

--- src/components/GalleryImages/GalleryImages.jsx
+++ src/components/GalleryImages/GalleryImages.jsx
@@ -6,16 +6,19 @@ import GalleryImage from '../GalleryImage/GalleryImage';
 const GalleryImages = ({ images, setImages, disSelectedAll }) => {
     return (
         <div className="grid grid-cols-5 gap-3 p-5">
-            <div className="border rounded-md col-span-2 row-span-2">
-                {images?.length > 0 && (
-                    <GalleryImage
-                        key={images[0].id}
-                        image={images[0]}
-                        setImages={setImages}
-                        disSelectedAll={disSelectedAll}
-                    />
-                )}
-            </div>
+            {images.length > 0 && (
+                <div className="border rounded-md col-span-2 row-span-2">
+                    {images?.length > 0 && (
+                        <GalleryImage
+                            key={images[0].id}
+                            image={images[0]}
+                            setImages={setImages}
+                            disSelectedAll={disSelectedAll}
+                        />
+                    )}
+                </div>
+            )}
+
             {images?.slice(1)?.map((image) => (
                 <GalleryImage
                     key={image?.id}
