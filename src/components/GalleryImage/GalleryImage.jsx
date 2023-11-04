174253dee2018a1bf1fbc59e6d58ccb2a8060738
dabsishJoy <dabasish@6sensehq.com>
2023-11-04 17:58:58 +0600

    conditional opcity for input box

--- src/components/GalleryImage/GalleryImage.jsx
+++ src/components/GalleryImage/GalleryImage.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const GalleryImage = ({ setImages, image }) => {
+    const ref = useRef();
     const toggleImage = (id) => {
         setImages((prevImages) =>
             prevImages.map((image) => {
@@ -12,22 +13,38 @@ const GalleryImage = ({ setImages, image }) => {
         );
     };
 
+    const toggleVisibilityCheckbox = (ref) => {
+        if (!ref.current.checked)
+            if (ref.current) {
+                if (ref.current.style.opacity == 1)
+                    ref.current.style.opacity = '0';
+                else {
+                    ref.current.style.opacity = '1';
+                }
+            }
+    };
+
     return (
         <div
             className="border rounded-md relative group"
             onClick={() => toggleImage(image.id)}
+            onMouseEnter={() => toggleVisibilityCheckbox(ref)}
+            onMouseLeave={() => toggleVisibilityCheckbox(ref)}
         >
             <input
                 type="checkbox"
                 name=""
+                ref={ref}
                 id={image?.id}
                 checked={image.checked}
-                className="absolute bg-white top-1 left-1 opacity-100 z-10"
+                className={`absolute bg-white top-1 left-1 ${
+                    image.checked ? 'opacity-100' : 'opacity-0'
+                } z-10`}
             />
             <img
                 src={image?.url}
                 alt="Gallery"
-                className="w-full h-auto rounded-md"
+                className="w-full h-auto rounded-md hover:opacity-100"
             />
             <div className="overlay absolute top-0 left-0 w-full h-full bg-gray-900 bg-opacity-30 transition-opacity duration-200 opacity-0 group-hover:opacity-80 rounded-md"></div>
         </div>
