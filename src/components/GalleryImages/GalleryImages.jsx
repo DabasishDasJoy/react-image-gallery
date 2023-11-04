3654171311d1f48ca6afecff07a967656a260d5a
dabsishJoy <dabasish@6sensehq.com>
2023-11-04 13:55:53 +0600

    created grid

--- src/components/GalleryImages/GalleryImages.jsx
+++ src/components/GalleryImages/GalleryImages.jsx
@@ -3,18 +3,42 @@ import React from 'react';
 const GalleryImages = () => {
     return (
         <div className="grid grid-cols-5">
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
-            <h1>Gallery</h1>
+            <div className="border border-red-400 rounded-sm col-span-2 row-span-2">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
+            <div className="border border-red-400 rounded-sm">
+                <h1>Gallery</h1>
+            </div>
         </div>
     );
 };
